bb1e9d1956fd569a814f22c24ee8829f017a0d4e
Troels Madsen <troels@mydahlmadsen.dk>
2018-04-05 09:50:37 +0200

    Configuration

--- WignDesign/routes/web.php
+++ WignDesign/routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('pages.home'); })->name('index');
+//Route::get('/new', function () { return view(''); })->name('createSign');
+//Route::get('/ask', function () { return view('layouts.index'); })->name('seeRequest');
+//Route::get('/signs', function () { return view('layouts.index'); })->name('seeRecent');
+Route::get('/about', function () { return view('pages.about'); })->name('about');
 
 Auth::routes();
 
