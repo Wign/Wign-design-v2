22c36e4cc106c8846628735e4848ec70e42b3f92
Kennik89 <kand89@gmail.com>
2019-09-19 18:33:21 +0200

    Update WordFactory.php
    sandsynligheden forøges for at gøre det lettere at fange fremmedsprog

--- database/factories/WordFactory.php
+++ database/factories/WordFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Word::class, function (Faker $faker) {
     $users = App\User::withTrashed()->inRandomOrder();
     $u1 = $users->first();
     $u2 = random_int(0, 4) == 0 ? $users->skip(1)->first() : $u1;
-    $l = random_int(0, 1000) > 0 ? \App\Language::whereCode('da_DK')->first() : \App\SignLanguage::inRandomOrder()->first();
+    $l = random_int(0, 100) > 0 ? \App\Language::whereCode('da_DK')->first() : \App\SignLanguage::inRandomOrder()->first();
 
     return [
         'literal' => $faker->unique()->word,
