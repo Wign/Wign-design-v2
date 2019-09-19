c0338751c20cb1ce5ccc73ee5233fd2f2aa0e49d
Kennik89 <kand89@gmail.com>
2019-09-19 18:20:04 +0200

    Update TranslationFactory.php
    Jeg skal bruge mere tid på at finde ud af hvad som giver bedst mening i forhold til om skal den høre under translation eller word. Jeg har leget med tanken hvis der skal være en beskrivelse af tegns udførelse, bør beskrivelsen foregå på det samme sprog, dvs. video frem for tekst. Det skal jeg arbejde videre med.

--- database/factories/TranslationFactory.php
+++ database/factories/TranslationFactory.php
@@ -3,12 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Translation::class, function (Faker $faker) {
-    $desc = factory(\App\Description::class)->create();
+    //$desc = factory(\App\Description::class)->create();
 
     return[
         //'word_id' => ,
         //'sign_id' => ,
-        'description_id' => $desc->id,
+        //'description_id' => $desc->id,
         'creator_id' => $desc->creator_id,
         'editor_id' => $desc->editor_id
         ];
