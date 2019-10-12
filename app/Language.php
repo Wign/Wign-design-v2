bb96d8167fb3dee03a246c0d0aa080696dac7c0a
Unknown <kand89@gmail.com>
2019-10-12 14:47:42 +0200

    Set the enum of written language to TEXT
    Some places was set as WRITE

--- app/Language.php
+++ app/Language.php
@@ -35,7 +35,7 @@ class Language extends Model
 
     public function words()
     {
-        return $this->hasMany(Word::class)->where('type', '=', 'WRITE');
+        return $this->hasMany(Word::class)->where('type', '=', 'TEXT');
     }
 
     public function signs()
