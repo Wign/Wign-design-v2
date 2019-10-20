2e2b5aeed41dbb1e5b51e39371a80dc1516de132
Troels Madsen <troels@madsen4.dk>
2019-10-20 10:48:19 +0200

    Adjusted a bit on video uuids, ensuring that they's valid videos

--- app/Providers/FakerProvider.php
+++ app/Providers/FakerProvider.php
@@ -79,56 +79,56 @@ class FakerProvider extends Base
         'fotografi', 'biologi', 'lande', 'farve', 'teknik', 'logik', 'figur', 'hovedstaed', 'danmark',
     ];
     protected static $videoUuidList = [
-        'v-992e8297-c561-4c4a-8eb1-55219dfb7263', 'v-4b3f8733-6054-40c2-975c-414899aff986',
-        'v-d6f2db50-9827-0137-cf4e-0adeb7a592e0', 'v-a65aa3d0-6891-4e7a-90d0-0b6bebf3c733',
-        'v-cf9c10e4-c2e0-41b8-99ab-9b4487885027', 'v-34be7360-9827-0137-cf4e-0adeb7a592e0',
-        'v-c98b3b40-9825-0137-cf4e-0adeb7a592e0', 'v-8c2de09b-6489-4e52-a0c3-1fa2a6b41d28',
-        'v-260afb50-9827-0137-cf4e-0adeb7a592e0', 'v-a27676d5-cbf8-400c-a3c9-2fdc3aa9b104',
-        'v-efec6373-91f6-4df8-89cf-e42c0729c189', 'v-597351d0-9827-0137-cf4e-0adeb7a592e0',
-        'v-04e704fd-0a81-48e3-a20a-344728d5a892', 'v-4de348c4-f373-4c62-8560-91c63f508494',
-        'v-09f7bc99-f430-451b-81f4-3f06727ba444', 'v-6458eea0-9826-0137-cf4f-0adeb7a592e0',
-        'v-b6a746eb-48cf-47ee-bab0-ae6f1ccdfabc', 'v-ce26c6c8-1e35-46e2-a7cb-7a64d94b7235',
-        'v-ef3c5cf5-848a-40f6-bc9f-0b20f2e7e624', 'v-fa283eb5-c599-4575-aab2-0e52df38728d',
-        'v-0aa6263e-cdc4-4024-b357-6a27a84a4afc', 'v-3a852d02-3e64-4a38-b0a6-007fe6eaf2c8',
-        'v-9bf9bad0-9824-0137-cf4e-0adeb7a592e0', 'v-c8c4acf0-9827-0137-cf4f-0adeb7a592e0',
-        'v-67193db1-76af-47cb-9a43-b5b76439de27', 'v-d006477e-8ad1-44cf-90df-22b06d0f98a8',
-        'v-b737b42b-977d-4fef-9e8c-30213d70f00d', 'v-6d4f9eb4-e89d-47e3-a6aa-8afdd855cea4',
-        'v-113ba950-9825-0137-cf4f-0adeb7a592e0', 'v-a8fc57c6-5d4e-4206-ae68-7680b68d1931',
-        'v-7d38d67b-3550-4e32-b3f9-e488cbe5e4f7', 'v-b6c62300-9826-0137-cf4e-0adeb7a592e0',
-        'v-72736ee0-9825-0137-cf4f-0adeb7a592e0', 'v-7db2ad7b-b692-4b87-9b3f-e0543bcd6bb9',
-        'v-845fc1c3-a225-40b0-94d0-9c3e885cfa99', 'v-f20ef8e6-dfb4-420e-9c53-867df5358eaa',
-        'v-5e188cea-7a5c-47a3-8330-3a2831b21220', 'v-53afb221-496e-4b9b-8d53-d0362b380fdc',
-        'v-e9ba81e1-b5bb-4d38-8022-94a0cb3805b6', 'v-4c4bf151-fef7-4cbe-8a65-bde4d8ebce50',
-        'v-217583da-9ffb-4296-9411-4541ce1a681f', 'v-3683d483-9d03-4cbb-98ed-2e3546152f9c',
-        'v-dbb05370-9824-0137-cf4f-0adeb7a592e0', 'v-b22952d4-3c6d-472b-a58f-1b4737a0026d',
-        'v-8586ffe0-9827-0137-b01d-02f6e3696dde', 'v-f7aabf4f-0ecc-405b-a428-db2f3cfd97ab',
-        'v-9c6de124-fb21-4836-9019-f80881c3394c', 'v-9c98e776-b751-447b-8cbf-88b47b8c3cb7',
-        'v-702c8c60-9826-0137-cf4f-0adeb7a592e0', 'v-95da1b82-18e4-4886-b1d4-c8046c02527b',
-        'v-16efaa53-2d6c-4aed-ba3f-f89a79d43ed7', 'v-7d858ca2-2f5e-4dd4-91eb-e405ac21d9c0',
-        'v-ff3aa520-9825-0137-cf4f-0adeb7a592e0', 'v-70e6bec2-7b1a-40f0-b64f-c0b8f61a0167',
-        'v-b0eb14a0-9825-0137-cf4f-0adeb7a592e0', 'v-e6a01140-9825-0137-b01c-02f6e3696dde',
-        'v-8c5aa820-9825-0137-cf4f-0adeb7a592e0', 'v-ee36a6f0-9825-0137-b01d-02f6e3696dde',
-        'v-4577b847-b0d7-4fb3-afa4-f809c93f4bd8', 'v-a06df768-92d5-4b5f-90d4-086b69cd3313',
-        'v-1fcb00b0-9825-0137-cf4e-0adeb7a592e0', 'v-b4b777e0-9826-0137-cf4e-0adeb7a592e0',
-        'v-d2419f5d-120c-4612-ae80-aedc2a01776d', 'v-956a6bd0-9827-0137-cf4f-0adeb7a592e0',
-        'v-f35f7ddb-4c65-49c5-b56b-6de3ffa37770', 'v-5107552f-0f5b-4b1f-b790-c80dd0f1ce0d',
-        'v-b74c8ad8-f209-4f1f-8ca1-025c4fdf8c24', 'v-d0f610fc-1ba2-4017-a178-bb146e92c8c3',
-        'v-0d7b1e60-9827-0137-cf4e-0adeb7a592e0', 'v-652683ff-6c4e-4d64-8bd1-6de5a74b7214',
-        'v-22709c26-be0e-42dc-a7ff-e474aa75abb8', 'v-8f51e4b0-9826-0137-b01c-02f6e3696dde',
-        'v-2cdd7c47-4a1c-419f-9bc8-e50a98e46b0c', 'v-18b64d1b-3963-47b5-86a1-ab10b4f223ea',
-        'v-03ec209e-60d8-4584-9bf0-0c7834498eb7', 'v-909273c0-9827-0137-b01c-02f6e3696dde',
-        'v-fec7b9d0-9826-0137-cf4e-0adeb7a592e0', 'v-83f37bb2-bb2e-4be9-919d-9a9748be4b80',
-        'v-835912d0-9826-0137-b01d-02f6e3696dde', 'v-ac246470-9826-0137-b01d-02f6e3696dde',
-        'v-dcb89a09-22ee-4899-bd4c-626a78c5e5d6', 'v-daa4e672-0a91-44ef-82ff-4cf2dfce6748',
-        'v-b4b796e0-9824-0137-cf4f-0adeb7a592e0', 'v-9a6c117f-6f48-4618-bf9f-ee9eaf3a4aaa',
-        'v-68034bb1-5f40-4c29-aec5-445f4b0b88ce', 'v-9577f818-86ef-43bd-b1c1-2ead95b7f3c1',
-        'v-d104431c-d8d5-4f53-bb4b-fb0b67f0b15b', 'v-ac6e30a0-9827-0137-b01d-02f6e3696dde',
-        'v-80f9cc87-ec26-49ec-b410-b87606b7a18f', 'v-88c8b470-9827-0137-b01c-02f6e3696dde',
-        'v-f85664aa-b0a4-4a13-8387-d22e05945fc1', 'v-56314cdd-3c45-4f9c-8c72-8311404aaa30',
-        'v-cf4db834-cf79-44e9-9400-ae03160a55cb', 'v-a3ef581b-9e67-40db-8433-4e59ad81883a',
-        'v-377f1a8a-1deb-40b3-8d25-3fb87df14a6f', 'v-05a64f40-9827-0137-cf4f-0adeb7a592e0',
-        'v-0039f79d-ca8d-415e-ba8d-c9e361d8b08a', 'v-b869f9c0-9824-0137-b01d-02f6e3696dde',
-        'v-a2730630-9827-0137-cf4e-0adeb7a592e0', 'v-4105e7a0-9827-0137-cf4e-0adeb7a592e0',
+        'v-7a5b8bd8-abd6-4cac-a7fe-1f152faa6fda', 'v-7949d64d-0615-41bd-80ab-cc0cc71065d4',
+        'v-d8b8bb90-9826-0137-cf4f-0adeb7a592e0', 'v-276bbbd8-daa1-4c7f-8037-693c911e4a89',
+        'v-3077c04f-2954-4dad-8c1c-8ea1d5743335', 'v-eb841c7c-cf12-4179-b93c-d2a9af8af309',
+        'v-2860e060-60a9-4924-bf29-eff3c9dfcff5', 'v-adf77620-9827-0137-b01c-02f6e3696dde',
+        'v-27c80978-8a42-4b85-b804-41f3bfc7e3cf', 'v-dfa9af14-086f-4c96-b4b7-17774b720783',
+        'v-6be5231c-c049-4878-8986-587af80f76e8', 'v-549efcc0-9826-0137-b01c-02f6e3696dde',
+        'v-194c9965-9a77-4596-83c0-0dc11a4dd9e6', 'v-bd96e9b0-9826-0137-cf4f-0adeb7a592e0',
+        'v-06929450-9826-0137-b01d-02f6e3696dde', 'v-eacc0432-ba29-4bcb-8ddb-60a0b5ee782f',
+        'v-dc5becb0-9824-0137-b01d-02f6e3696dde', 'v-a4975f39-61fc-4506-8f58-f652fa967cc7',
+        'v-31582398-ac9d-4fa9-a591-fc39658ee8be', 'v-a588af20-9824-0137-b01d-02f6e3696dde',
+        'v-4b2b2a20-9826-0137-cf4f-0adeb7a592e0', 'v-9b98bca0-9824-0137-b01d-02f6e3696dde',
+        'v-855821f3-aa7a-4bc9-8d04-f7ee95789219', 'v-7ed1ef5e-b576-45b8-a320-1310ac1d3fe7',
+        'v-ae638250-9826-0137-b01d-02f6e3696dde', 'v-98f9ea63-b356-4b0c-9958-77f498a6cfe4',
+        'v-917a0b20-9825-0137-cf4f-0adeb7a592e0', 'v-b76dcaec-d292-42d5-8588-c26f39284454',
+        'v-0b46c600-5b2f-4d12-87ca-0c72390b316a', 'v-d430dbfd-9968-4b89-b38f-c8ec93664564',
+        'v-48ae61b6-bfdd-4735-ab1c-8c2ed94e0837', 'v-c28fabd0-9825-0137-b01c-02f6e3696dde',
+        'v-86e998de-00dc-4011-b6ed-d07f923d6f99', 'v-c2293790-9825-0137-cf4f-0adeb7a592e0',
+        'v-17d71050-9827-0137-cf4e-0adeb7a592e0', 'v-4b809377-1237-4bff-8d5a-2b5b7cc87a0a',
+        'v-cb71a105-14b4-4131-a45f-5b4e019e9af6', 'v-d5e6c9a0-55db-425f-9f98-a217500deda0',
+        'v-e8f59e10-9826-0137-cf4f-0adeb7a592e0', 'v-9e83d4f0-9824-0137-cf4e-0adeb7a592e0',
+        'v-597cd5c4-725c-4cf7-ac0c-526e7e9c0f68', 'v-109a8ae0-9827-0137-b01d-02f6e3696dde',
+        'v-59a969ab-86ec-4107-ba41-c7c0b5bd3f0c', 'v-dd3441a2-ef4d-4911-b5e0-1e6af341495c',
+        'v-9e4ccd50-9826-0137-b01d-02f6e3696dde', 'v-c1e33030-9825-0137-cf4f-0adeb7a592e0',
+        'v-8861d4e4-4f9e-4fab-8aee-40c4e5b7a71f', 'v-b5d27600-9825-0137-cf4f-0adeb7a592e0',
+        'v-f7058b95-724f-4867-a4c4-55b80fa57cc0', 'v-a648dc5f-e1e2-4a9a-8c5a-c745d7028fce',
+        'v-252e048b-afc8-452a-a897-3c4ed87db5f6', 'v-34ec2c2b-0ccf-482b-8c01-149f4f33e2b5',
+        'v-1b4f6147-7ae5-442c-8cf6-94bf8f58125b', 'v-74928969-115c-4e62-8fa0-35fb874f4dd1',
+        'v-372efdb0-9825-0137-b01c-02f6e3696dde', 'v-d3286620-9825-0137-cf4f-0adeb7a592e0',
+        'v-83379a07-f160-466d-a7fb-c4eea4132a54', 'v-89b8cdbe-9128-44a3-ae9f-7848b02406ec',
+        'v-4e146170-9825-0137-cf4e-0adeb7a592e0', 'v-165ba665-1cac-41f7-8102-ad21a14b2b7e',
+        'v-afc40780-9825-0137-cf4e-0adeb7a592e0', 'v-ca29fe7f-73b8-460a-a5b8-7d357415019e',
+        'v-923c7270-9825-0137-b01d-02f6e3696dde', 'v-a684a580-9825-0137-b01c-02f6e3696dde',
+        'v-fb4837be-e0ad-4bcd-837a-7321129f0e7f', 'v-c30298a0-9825-0137-cf4e-0adeb7a592e0',
+        'v-239a1666-25fc-4e6c-8d56-c8b5e3926a38', 'v-b084b510-9825-0137-b01c-02f6e3696dde',
+        'v-a3a80c36-17fb-4d91-80da-10baa39e0da0', 'v-30c3feb0-9827-0137-cf4e-0adeb7a592e0',
+        'v-60987164-73c1-4c70-8bec-f04b6ec3cc81', 'v-cdf4b943-1a76-4c61-a7ba-0b505aa4aaa5',
+        'v-341c6b10-9825-0137-cf4f-0adeb7a592e0', 'v-7ccc4fb0-9826-0137-b01d-02f6e3696dde',
+        'v-40de1290-9825-0137-cf4f-0adeb7a592e0', 'v-2837f840-9826-0137-cf4e-0adeb7a592e0',
+        'v-b8501450-9827-0137-cf4e-0adeb7a592e0', 'v-0250ec60-9826-0137-cf4f-0adeb7a592e0',
+        'v-f9345b60-9826-0137-cf4f-0adeb7a592e0', 'v-877eb380-9827-0137-cf4e-0adeb7a592e0',
+        'v-4921b59a-6ea4-43e3-9683-49674eda793d', 'v-c40bec10-9827-0137-b01d-02f6e3696dde',
+        'v-d33ba847-2f89-4667-ab50-f6ae33a3c535', 'v-da107d9e-c5d3-4198-87e4-a7b426f4c52f',
+        'v-0f8df4b0-9826-0137-b01c-02f6e3696dde', 'v-595704b1-511e-4d4d-b7b2-7cb61a68756e',
+        'v-ea263749-59fb-44a8-bc14-fb992ed3b904', 'v-c2a285aa-9d98-4bb2-9bb7-f4fb3a488376',
+        'v-4154e220-de2d-469e-bda6-3c852c262d7f', 'v-f1399a30-9826-0137-cf4f-0adeb7a592e0',
+        'v-fb58c310-9826-0137-b01c-02f6e3696dde', 'v-5e5f0a48-81e5-4cab-8829-0bf5fbe79337',
+        'v-d2a347eb-fee1-43c7-b599-a92af4cd48f6', 'v-b8bc2409-50da-456d-aa28-5353bd9dc09e',
+        'v-f8071ff0-9824-0137-cf4e-0adeb7a592e0', 'v-fdee85e0-9825-0137-b01d-02f6e3696dde',
+        'v-6b11054b-5959-460d-b665-bd8cbe91ae8e', 'v-5117ee20-9825-0137-b01c-02f6e3696dde',
+        'v-88beb0ba-fefb-4042-8dc2-13ed35b08d23', 'v-4b887280-809a-45d2-9c4a-c1a9643e736c',
     ];
 
     /**
@@ -147,7 +147,7 @@ class FakerProvider extends Base
      */
     public function wignLiterals(int $min = null, int $max = null)
     {
-        if (! $min && ! $max) {
+        if ( ! $min && ! $max) {
             $number = $this->biasedRandom([50, 30, 20]);
         } else {
             $number = rand($min, $max);
@@ -174,7 +174,7 @@ class FakerProvider extends Base
 
     public function textWithHashtag(int $numWord = 10, int $numHash = 3)
     {
-        $words = $this::randomElements(static::$wordList, $numWord, true);
+        $words  = $this::randomElements(static::$wordList, $numWord, true);
         $hashes = $this->hashtag($numHash);
 
         return implode(' ', self::shuffle(array_merge($words, $hashes)));
@@ -182,7 +182,7 @@ class FakerProvider extends Base
 
     private function biasedRandom(array $bias): int
     {
-        $count = rand(0, 99);
+        $count  = rand(0, 99);
         $number = 0;
         while ($count >= 0) {
             $count -= array_shift($bias);
