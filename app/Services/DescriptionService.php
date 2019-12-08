20981ad8e73cf7b0d1d33dc24c8ba5850ff9ab4e
Kennik89 <kand89@gmail.com>
2019-12-08 17:32:30 +0100

    Translation features (#20)
    * Apply fixes from StyleCI (#19)
    
    * Draft of translation processes
    
    * Translation features implemented
    
    * Translation mutations updated to working version.
    Requires extra work on the controller to get it to work.
    
    * Perhaps a fix to style.ci?
    
    * Apply fixes from StyleCI

---
+++ app/Services/DescriptionService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Description;
+use App\User;
+use Illuminate\Http\Request;
+
+class DescriptionService
+{
+    public function makeDescription(Request $request, User $user)
+    {
+        $desc = $this->newDescription($request, $user);
+
+        return $desc;
+    }
+
+    public function editDescription(Request $request, \App\Translation $translation, ?\Illuminate\Contracts\Auth\Authenticatable $user)
+    {
+        if ($request->input('text') == null) {
+            return null;
+        }
+
+        $newDesc = $this->newDescription($request, $user);
+
+        return $newDesc;
+    }
+
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return Description|\Illuminate\Database\Eloquent\Model
+     */
+    public function newDescription(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'text'       => 'nullable|string',
+        ]);
+
+        $desc = Description::make([
+            'text' => $request->input('text'),
+            'creator_id' => $user->id,
+            'editor_id' => $user->id,
+        ]);
+
+        return $desc;
+    }
+}
