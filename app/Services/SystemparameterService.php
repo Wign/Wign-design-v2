7578c0b21a687c1f882c99e41164143145b3b0f3
Troels Madsen <troels@madsen4.dk>
2019-12-08 16:50:38 +0000

    Apply fixes from StyleCI

--- app/Services/SystemparameterService.php
+++ app/Services/SystemparameterService.php
@@ -1,22 +1,21 @@
 <?php
 
-
 namespace App\Http\Controllers;
-
 
 use App\Systemparameter;
 
-class SystemparameterService {
-
-    public function getParameter(string $type): int {
+class SystemparameterService
+{
+    public function getParameter(string $type): int
+    {
         return Systemparameter::where('type', $type)->value('value');
     }
 
-    public function newParameter(string $type, int $value) {
+    public function newParameter(string $type, int $value)
+    {
         Systemparameter::create([
             'type' => $type,
             'value' => $value,
         ]);
     }
-
 }
