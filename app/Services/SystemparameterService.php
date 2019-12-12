a7d2c954d61b017badf6b3e8dbba734823f99221
Kenneth Andersen <kand89@gmail.com>
2019-12-12 21:36:09 +0100

    reformating code

--- app/Services/SystemparameterService.php
+++ app/Services/SystemparameterService.php
@@ -1,27 +1,27 @@
 <?php
 
-
 namespace App\Http\Controllers;
-
 
 use App\Systemparameter;
 
-class SystemparameterService {
-
-    public function getParameter(string $type, string $key) {
+class SystemparameterService
+{
+    public function getParameter(string $type, string $key)
+    {
         $value = Systemparameter::where('key', $key)->value('value');
 
         switch ($type) {
-            case "STRING" :
+            case 'STRING':
                 return (string) $value;
-            case "INTEGER" :
+            case 'INTEGER':
                 return (int) $value;
-            default :
+            default:
                 return $value;
         }
     }
 
-    public function newParameter(string $key, string $type, $value) {
+    public function newParameter(string $key, string $type, $value)
+    {
         Systemparameter::create([
             'key' => $key,
             'type' => $type,
@@ -29,7 +29,8 @@ class SystemparameterService {
         ]);
     }
 
-    public function updateParameter(string $key, string $type, $value) {
+    public function updateParameter(string $key, string $type, $value)
+    {
         $parameter = Systemparameter::find($key);
 
         if ($type != null) {
