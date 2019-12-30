d9f7c29d1b6343715e2ad32acd12442c5b8a0791
Kenneth Andersen <kand89@gmail.com>
2019-12-30 12:55:18 +0100

    Add lang.error.php for error message on front

---
+++ resources/lang/da/error.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'upload'    => 'Upload et tegn',
+    'requested' => 'Efterlyste tegn',
+    'recent'    => 'Seneste tegn',
+    'all'       => 'Alle tegn',
+    'about'     => 'Om Wign',
+    'login'     => 'Login',
+    'register'  => 'Opret',
+    'profile'   => 'Profil',
+    'logo'      => 'Wign logo',
+];
