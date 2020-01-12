787d872dc7dac8e17bbf49c61696f3dedd1f7e3a
Kenneth Andersen <kand89@gmail.com>
2020-01-12 11:24:57 +0100

    Setup of test
    - test with graphql query
    - DB migration for test

---
+++ tests/Unit/RequestToggleTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Sign;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use InitSeeder;
+use Tests\TestCase;
+
+class RequestToggleTest extends TestCase
+{
+    use DatabaseTransactions;
+    use DatabaseMigrations;
+
+    public function setUp() :void
+    {
+        parent::setUp();
+        $this->seed(InitSeeder::class);
+        
+        factory(User::class)->create();
+        factory(Sign::class)->create();
+    }
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testRequestToggle()
+    {
+        $user = User::first();
+        $sign = Sign::first();
+
+        $response = $this->postGraphQL([
+            'query' => '
+            mutation toggleLike($userId: ID!, $signId: ID!) {
+                ToggleLike(userId: $userId, signId: $signId) {
+                    id
+                }
+            }
+        ',
+            'variables' => [
+                'userId' => $user->id,
+                'signId' => $sign->id,
+            ],
+        ]);
+        //dd($response);
+
+    }
+}
